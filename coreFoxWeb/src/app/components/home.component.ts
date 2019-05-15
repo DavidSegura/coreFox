47037afb90a8c15460bad6fe16535f43dfdc8a41
Arges Nolan <wolf.arges@gmail.com>
2019-05-15 18:04:14 +0200

    Statistic Cards

--- coreFoxWeb/src/app/components/home.component.ts
+++ coreFoxWeb/src/app/components/home.component.ts
@@ -5,7 +5,16 @@ import { Component } from "@angular/core";
     templateUrl: "../views/home.component.html"
 })
 export class HomeComponent{
-    ngInit(){
-        console.log("ngInit Home Component");
+    public apps = [];
+
+    constructor(){}
+
+    ngOnInit(){
+        this.apps.push(
+            {name: "codeFox", icon: "https://cdn.icon-icons.com/icons2/1088/PNG/512/1485282143-adobe-illustrator-cc-creative-cloud_78298.png"},
+            {name: "ForceOfWill", icon: "https://cdn1.iconfinder.com/data/icons/adobe-3/512/Photoshop.png"},
+            {name: "dataFox", icon: "https://mir-s3-cdn-cf.behance.net/project_modules/source/6b7d0325768845.5634a73de86d0.png"}, 
+            {name: "Legend of Fate", icon: "https://lh3.googleusercontent.com/1JJMfwHTCT-wDIxciN2XFCqQV7neyoJ1DhXQIuEkPJ5mxwiYDiAqdeZT8UxLEfW8AQ"});
+        console.log(this.apps);
     }
 }
